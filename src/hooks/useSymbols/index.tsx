bccf71fba81b1a68e41183bc0193495da5b55150
Louis Young <me@louisyoung.co.uk>
2021-03-22 19:00:58 +0000

    Build tag searching logic

--- src/hooks/useSymbols/index.tsx
+++ src/hooks/useSymbols/index.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { getSymbolsByCategory, getSymbolsBySearchQuery } from "./utilities";
 import type { Symbol } from "../../types/symbol";
 import type { SymbolCategories } from "../../types/symbolCategories";
-import initialSymbols from "../../data/symbols.json";
+import initialSymbols from "../../data/database.json";
 
 export const useSymbols = (searchQuery: string, category: SymbolCategories) => {
   const [symbols, setSymbols] = useState<Symbol[]>(initialSymbols);
--- src/hooks/useSymbols/utilities.ts
+++ src/hooks/useSymbols/utilities.ts
@@ -1,5 +1,8 @@
-import symbols from "../../data/symbols.json";
+import database from "../../data/database.json";
+import type { Symbol } from "../../types/symbol";
 import type { SymbolCategories } from "../../types/symbolCategories";
+
+const symbols: Symbol[] = database;
 
 export const getSymbolsByCategory = (category: SymbolCategories) => {
   const symbolsByCategory = symbols.filter(({ categories }) => categories.includes(category));
@@ -8,12 +11,14 @@ export const getSymbolsByCategory = (category: SymbolCategories) => {
 };
 
 export const getSymbolsBySearchQuery = (searchQuery: string) => {
-  const symbolsBySearchQuery = symbols.filter(({ name, symbol }) => {
+  const symbolsBySearchQuery = symbols.filter(({ name, symbol, tags }) => {
     const searchQueryMatchesName = name.toLowerCase().includes(searchQuery.toLowerCase());
 
     const searchQueryMatchesSymbol = symbol.includes(searchQuery);
 
-    return searchQueryMatchesName || searchQueryMatchesSymbol;
+    const searchQueryMatchesTag = tags && tags.some((tag) => tag.toLowerCase().includes(searchQuery.toLowerCase()));
+
+    return searchQueryMatchesName || searchQueryMatchesSymbol || searchQueryMatchesTag;
   });
 
   return symbolsBySearchQuery;
