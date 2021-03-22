b8bb2ea1c1488cb41bb5bfaedeb55eec95ea4402
Louis Young <me@louisyoung.co.uk>
2021-03-22 20:50:08 +0000

    Build search clearing functionality

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -7,8 +7,13 @@ export const Search = ({ searchQuery, onSearchQueryChange }: SearchProps) => {
 
     onSearchQueryChange(newSearchQuery);
   };
+
+  const handleClearSearchQueryClick = () => {
+    onSearchQueryChange("");
+  };
+
   return (
-    <form className="w-full md:w-96 max-w-3xl mr-auto">
+    <form className="w-full md:w-96 max-w-3xl mr-auto relative">
       <input
         type="text"
         name="search"
@@ -19,6 +24,15 @@ export const Search = ({ searchQuery, onSearchQueryChange }: SearchProps) => {
         onChange={handleChange}
         className="p-3 w-full shadow-sm rounded-sm placeholder-gray-500"
       />
+
+      {searchQuery && (
+        <button
+          className="absolute top-1/2 right-2 transform -translate-y-1/2 text-gray-600 text-sm font-bold w-8 h-8 rounded-full bg-blue-50 transition hover:bg-blue-100"
+          onClick={handleClearSearchQueryClick}
+        >
+          ✕
+        </button>
+      )}
     </form>
   );
 };
